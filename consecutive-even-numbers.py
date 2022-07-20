c69ae0546c7db976956220a5cd1c642c1544db1c
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-20 11:54:42 +0200

    Update consecutive-even-numbers.py

--- consecutive-even-numbers.py
+++ consecutive-even-numbers.py
@@ -6,9 +6,21 @@ while a<=20:
     a+=2 #zwiększanie wartości zmiennej a o 2. #to samo co a=a+2
     
     
-### OR
+#   OR
 
 for x in range(2,21,2):#od 2 do 21(bo 1 jest nieparzyste) i przeskok co 2
     print (x)
+
+    
+#   OR
+    
+a=0
+
+while a<=20:
+    a+=1
+    if a%2!=0:
+        continue
+    #print w 1 linii:
+    print(a,end=" ") 
     
     
